b844761b430008c67b5c4599daa0b72f3b63e6e8
Bilal-018 <bilalimran018@gmail.com>
2024-08-11 16:53:36 +0500

    testfix0071.1

--- src/services/interceptors.tsx
+++ src/services/interceptors.tsx
@@ -83,7 +83,7 @@ jwtLeave.interceptors.response.use(
       const authDataString = sessionStorage.getItem('token');
       if (!authDataString) {
         // Handle the case where authData is null
-        return Promise.reject(error);
+        return Promise.reject(new Error('Authentication failed', { cause: error }));
       }
 
       const authData = JSON.parse(authDataString);
@@ -105,7 +105,7 @@ jwtLeave.interceptors.response.use(
       error.config.headers.Authorization = 'Bearer ' + apiResponse.data.token;
       return axios(error.config);
     } else {
-      return Promise.reject(error);
+      return Promise.reject(new Error('Error', { cause: error }));
     }
   }
 );
