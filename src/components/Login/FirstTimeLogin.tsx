054ddf03ca948a1eb479d50e748174f9fc8b26b6
Bilal-018 <bilalimran018@gmail.com>
2024-08-11 14:45:58 +0500

    testfix0059.1

--- src/components/Login/FirstTimeLogin.tsx
+++ src/components/Login/FirstTimeLogin.tsx
@@ -208,7 +208,7 @@ function FirstTimeLogin({ setStep }: any) {
               width: '80%',
               mt: 4,
             }}
-            onClick={() => { updateNewPassword(); }}
+            onClick={async () => { await updateNewPassword(); }}
           >
             {t('Reset Password')}
           </Button>
